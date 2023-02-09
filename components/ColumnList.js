5868a1fbccf187eeecf384cd40c2062cec401df8
jake@seahag.onmicrosoft.com <jake@seahag.onmicrosoft.com>
2023-02-09 13:56:44 -0600

    tt

---
+++ components/ColumnList.js
@@ -0,0 +1,13 @@
+import { Component } from './Component.js';
+export class ColumnList extends Component {
+  constructor() {
+    super('column-list', {
+      templateName: 'column-list',
+      elementProperties: {},
+    });
+    
+  };
+  
+  // get prop() { return this.#prop };
+  // set prop(v) { this.#prop = v };
+}
